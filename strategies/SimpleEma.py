fb626c35cd5946ea2e673da171b9e33e72b716c2
Xaemiphor <xaemiphor@users.noreply.github.com>
2025-01-02 16:51:04 -0800

    strategies: changing up experiments

---
+++ strategies/SimpleEma.py
@@ -0,0 +1,120 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these imports ---
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+from pandas import DataFrame
+from typing import Optional, Union
+
+from freqtrade.strategy import (
+    IStrategy,
+    Trade,
+    Order,
+    PairLocks,
+    informative,  # @informative decorator
+    # Hyperopt Parameters
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IntParameter,
+    RealParameter,
+    # timeframe helpers
+    timeframe_to_minutes,
+    timeframe_to_next_date,
+    timeframe_to_prev_date,
+    # Strategy helper functions
+    merge_informative_pair,
+    stoploss_from_absolute,
+    stoploss_from_open,
+)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+from technical import qtpylib
+
+
+class SimpleEma(IStrategy):
+
+    INTERFACE_VERSION = 3
+
+    can_short: bool = False
+
+    minimal_roi = {
+        # "120": 0.0,  # exit after 120 minutes at break even
+        "60": 0.01,
+        "30": 0.02,
+        "0": 0.04,
+    }
+
+    stoploss = -0.10
+
+    trailing_stop = False
+
+    timeframe = "5m"
+
+    process_only_new_candles = False
+
+    use_exit_signal = True
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+    # Number of candles the strategy requires before producing valid signals
+    startup_candle_count: int = 200
+
+    # Optional order type mapping.
+    order_types = {
+        "entry": "limit",
+        "exit": "limit",
+        "stoploss": "market",
+        "stoploss_on_exchange": False,
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {"entry": "GTC", "exit": "GTC"}
+
+    plot_config = {
+        "main_plot": {
+            "tema": {"color":"white"},
+            "ema5": {"color":"green"},
+            "ema21": {"color":"blue"},
+        },
+        "subplots": {
+        },
+    }
+
+    def informative_pairs(self):
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['ema5'] = ta.EMA(dataframe, timeperiod=5)
+        dataframe['ema21'] = ta.EMA(dataframe, timeperiod=21)
+
+        # TEMA - Triple Exponential Moving Average
+        dataframe["tema"] = ta.TEMA(dataframe, timeperiod=9)
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (qtpylib.crossed_below(dataframe["ema5"], dataframe["ema21"]))
+                & (dataframe["volume"] > 0)  # Make sure Volume is not 0
+            ),
+            "enter_long",
+        ] = 1
+
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (qtpylib.crossed_above(dataframe["ema5"], dataframe["ema21"]))
+                & (dataframe["volume"] > 0)  # Make sure Volume is not 0
+            ),
+            "exit_long",
+        ] = 1
+
+        return dataframe
---
+++ strategies/SimpleSma.py
@@ -0,0 +1,120 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these imports ---
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+from pandas import DataFrame
+from typing import Optional, Union
+
+from freqtrade.strategy import (
+    IStrategy,
+    Trade,
+    Order,
+    PairLocks,
+    informative,  # @informative decorator
+    # Hyperopt Parameters
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IntParameter,
+    RealParameter,
+    # timeframe helpers
+    timeframe_to_minutes,
+    timeframe_to_next_date,
+    timeframe_to_prev_date,
+    # Strategy helper functions
+    merge_informative_pair,
+    stoploss_from_absolute,
+    stoploss_from_open,
+)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+from technical import qtpylib
+
+
+class SimpleSma(IStrategy):
+
+    INTERFACE_VERSION = 3
+
+    can_short: bool = False
+
+    minimal_roi = {
+        # "120": 0.0,  # exit after 120 minutes at break even
+        "60": 0.01,
+        "30": 0.02,
+        "0": 0.04,
+    }
+
+    stoploss = -0.10
+
+    trailing_stop = False
+
+    timeframe = "5m"
+
+    process_only_new_candles = False
+
+    use_exit_signal = True
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+    # Number of candles the strategy requires before producing valid signals
+    startup_candle_count: int = 200
+
+    # Optional order type mapping.
+    order_types = {
+        "entry": "limit",
+        "exit": "limit",
+        "stoploss": "market",
+        "stoploss_on_exchange": False,
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {"entry": "GTC", "exit": "GTC"}
+
+    plot_config = {
+        "main_plot": {
+            "tema": {"color":"white"},
+            "sma5": {"color":"green"},
+            "sma21": {"color":"blue"},
+        },
+        "subplots": {
+        },
+    }
+
+    def informative_pairs(self):
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['sma5'] = ta.SMA(dataframe, timeperiod=5)
+        dataframe['sma21'] = ta.SMA(dataframe, timeperiod=21)
+
+        # TEMA - Triple Exponential Moving Average
+        dataframe["tema"] = ta.TEMA(dataframe, timeperiod=9)
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (qtpylib.crossed_below(dataframe["sma5"], dataframe["sma21"]))
+                & (dataframe["volume"] > 0)  # Make sure Volume is not 0
+            ),
+            "enter_long",
+        ] = 1
+
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (qtpylib.crossed_above(dataframe["sma5"], dataframe["sma21"]))
+                & (dataframe["volume"] > 0)  # Make sure Volume is not 0
+            ),
+            "exit_long",
+        ] = 1
+
+        return dataframe
