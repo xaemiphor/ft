fd8e1303135cfb3267198b4e6dbda03216c73e11
Xaemiphor <xaemiphor@users.noreply.github.com>
2025-01-03 16:23:05 -0800

    moar sma

---
+++ strategies/SMAOffsetCrossIn.py
@@ -0,0 +1,134 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these imports ---
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+from pandas import DataFrame
+from typing import Optional, Union
+
+from freqtrade.strategy import (
+    IStrategy,
+    Trade,
+    Order,
+    PairLocks,
+    informative,  # @informative decorator
+    # Hyperopt Parameters
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IntParameter,
+    RealParameter,
+    # timeframe helpers
+    timeframe_to_minutes,
+    timeframe_to_next_date,
+    timeframe_to_prev_date,
+    # Strategy helper functions
+    merge_informative_pair,
+    stoploss_from_absolute,
+    stoploss_from_open,
+)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+from technical import qtpylib
+from functools import reduce
+
+ma_types = {
+    'SMA': ta.SMA,
+    'EMA': ta.EMA,
+}
+
+class SMAOffsetCrossIn(IStrategy):
+
+    INTERFACE_VERSION = 3
+
+    can_short: bool = False
+
+    minimal_roi = {
+        # "120": 0.0,  # exit after 120 minutes at break even
+        # "60": 0.01,
+        # "30": 0.02,
+        # "0": 0.04,
+    }
+
+    stoploss = -0.50
+
+    trailing_stop = False
+
+    timeframe = "5m"
+
+    process_only_new_candles = False
+
+    use_exit_signal = True
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+    # Hyperoptable parameters
+    base_nb_candles_buy = IntParameter(5, 80, default=30, space='buy')
+    base_nb_candles_sell = IntParameter(5, 80, default=30, space='sell')
+    low_offset = DecimalParameter(0.8, 0.99, default=0.950, space='buy')
+    high_offset = DecimalParameter(0.8, 1.1, default=1.010, space='sell')
+    buy_trigger = CategoricalParameter(ma_types.keys(), default='SMA', space='buy')
+    sell_trigger = CategoricalParameter(ma_types.keys(), default='EMA', space='sell')
+
+    # Number of candles the strategy requires before producing valid signals
+    startup_candle_count: int = 200
+
+    # Optional order type mapping.
+    order_types = {
+        "entry": "limit",
+        "exit": "limit",
+        "stoploss": "market",
+        "stoploss_on_exchange": False,
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {"entry": "GTC", "exit": "GTC"}
+
+    plot_config = {
+        "main_plot": {
+            "tema": {"color":"white"},
+            "close": {"color":"blue"},
+            "ma_offset_buy": {"color":"red"},
+            "ma_offset_sell": {"color":"green"},
+        },
+        "subplots": {
+        },
+    }
+
+    def informative_pairs(self):
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe["tema"] = ta.TEMA(dataframe, timeperiod=9)
+        dataframe['ma_offset_buy'] = ma_types[self.buy_trigger.value](dataframe, int(self.base_nb_candles_buy.value)) * self.low_offset.value
+        dataframe['ma_offset_sell'] = ma_types[self.sell_trigger.value](dataframe, int(self.base_nb_candles_sell.value)) * self.high_offset.value
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = []
+        conditions.append(dataframe["volume"] > 0)
+        conditions.append(dataframe['close'] < dataframe['ma_offset_buy'])
+        conditions.append(qtpylib.crossed_above(dataframe['close'],dataframe['ma_offset_buy']))
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x & y, conditions),
+                'enter_long'] = 1
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = []
+        conditions.append(dataframe["volume"] > 0)
+        conditions.append(dataframe['close'] > dataframe['ma_offset_sell'])
+        conditions.append(qtpylib.crossed_below(dataframe['close'],dataframe['ma_offset_sell']))
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x & y, conditions),
+                'exit_long'] = 1
+        return dataframe
